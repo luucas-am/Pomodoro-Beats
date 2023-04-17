dfa1fc6cc3838412ea33059d484b6c60c58a2302
Lucas Andrade <lucas.andrade7899@gmail.com>
2023-04-17 18:50:08 -0300

    feat: Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id='footer'>
+        <p>Made with 🤍 by <a href="https://github.com/luucas-am">Lucas Andrade</a></p>
+    </footer>
+  )
+}
+
+export default Footer
